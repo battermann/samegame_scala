d39075ad3f360a4a6f21348a7582a65b7a729834
Leif Battermann <leifbattermann@gmail.com>
2017-03-13 09:33:28 +0100

    cli

--- src/main/scala/com/example/Application.scala
+++ src/main/scala/com/example/Application.scala
@@ -7,12 +7,13 @@ import cats.data._
 import cats.implicits._
 import com.example.eventstore._
 import com.example.samegame._
+import com.typesafe.config.{ConfigFactory, ConfigValueFactory}
 
 import scala.annotation.tailrec
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration.Duration
 import scala.concurrent.{Await, Future}
-import scala.util.Try
+import scala.util.{Random, Try}
 
 object Application {
 
@@ -46,17 +47,68 @@ object Application {
   }
 
   def main(args: Array[String]): Unit = {
-    implicit val akkaSystem = akka.actor.ActorSystem()
+    val config = ConfigFactory.load()
+      .withValue("akka.loglevel", ConfigValueFactory.fromAnyRef("OFF"))
+      .withValue("akka.stdout-loglevel", ConfigValueFactory.fromAnyRef("OFF"))
+
+    implicit val akkaSystem = akka.actor.ActorSystem("same-game-akka-system", config)
+
+    val store = EventStore(InMemoryEventStore.appendToStream, InMemoryEventStore.readFromStream)
+    //val store = EventStore(RedisEventStore.appendToStream("localhost", 6379, "samegame:commits"), RedisEventStore.readFromStream("localhost", 6379, "samegame:commits"))
+    val handle = CommandHandling.handle(store, Some(e => println(e))) _
+
+    val rnd = Random
 
     @tailrec
     def loop(cmd: Option[CliCmd]): Unit = {
       cmd match {
+
         case Some(Exit) => ()
+
         case Some(NewLine) =>
           val cmd = promptAndGetInput()
           loop(cmd)
+
+        case Some(StartGame(width, height)) =>
+          val id = GameId(UUID.randomUUID())
+          val columns = (1 to width)
+            .map(_ => Column((1 to height)
+              .map(_ => Filled(Color(rnd.nextInt(5))))
+              .toList))
+            .toList
+
+          val result =
+            EitherT(Future.successful(Board.create(columns)))
+              .map(StartNewGame)
+              .flatMap(cmd => handle(id, cmd))
+
+          Await.result(result.value, Duration.Inf)
+            .fold(
+              err => println(s"[ERROR] $err"),
+              _ => println(s"$id"))
+
+          val cmd = promptAndGetInput()
+          loop(cmd)
+
+        case Some(Play(id, column, row)) =>
+          val maybeGameId = Try(UUID.fromString(id))
+            .toOption
+            .toRight(InvalidGameIdFormat)
+            .map(GameId)
+
+          val result = EitherT(Future.successful(maybeGameId))
+            .map(gameId => handle(gameId, RemoveGroup(Position(column, row))))
+
+          Await.result(result.value, Duration.Inf)
+            .fold(
+              err => println(s"[ERROR] $err"),
+              _ => ())
+
+          val cmd = promptAndGetInput()
+          loop(cmd)
+
         case _ =>
-          println("[error] unknown input")
+          println("unknown input")
           val cmd = promptAndGetInput()
           loop(cmd)
       }
