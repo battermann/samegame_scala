8986698aa778b0fe16a85c03ab4384e8d28a68c8
Leif Battermann <leifbattermann@gmail.com>
2017-03-20 23:27:25 +0100

    merge

--- src/main/scala/com/example/samegame/Events.scala
+++ src/main/scala/com/example/samegame/Events.scala
@@ -1,6 +1,6 @@
 package com.example.samegame
 
 sealed trait Event
-final case class GameStarted(id: GameId, board: Board) extends Event
-final case class GroupRemoved(id: GameId, board: Board, score: Int) extends Event
+final case class GameStarted(id: GameId,board: Board) extends Event
+final case class GroupRemoved(id: GameId, position: Position, board: Board, score: Int) extends Event
 final case class GameFinished(id: GameId) extends Event
